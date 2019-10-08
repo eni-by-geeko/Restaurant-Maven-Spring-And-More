5e929981824214c9327f4f60b6dd05d7bb303b5e
Kchriss <52273624+Kchriss@users.noreply.github.com>
2019-10-08 11:56:28 +0200

    Base Projet

---
+++ restaurant/restaurant-model/src/main/java/org/bygecko/training/model/App.java
@@ -0,0 +1,13 @@
+package org.bygecko.training.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
